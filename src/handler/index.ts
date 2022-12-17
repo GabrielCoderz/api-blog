48f686be6b647b020d6823c4baff326dee3ee222
GabrielCoderz <biel.s.maia@hotmail.com>
2022-12-17 13:23:01 -0300

    first

---
+++ src/handler/index.ts
@@ -0,0 +1,12 @@
+import { APIGatewayProxyEvent, APIGatewayProxyResultV2, Handler } from 'aws-lambda'
+import * as _ from 'lodash';
+
+
+export const handler: Handler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResultV2> => {
+    const response = {
+        statusCode: 200,
+        body: JSON.stringify('Hello from Lambda again!')
+    }
+
+    return response;
+}
