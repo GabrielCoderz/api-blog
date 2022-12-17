0fab9d1471fa1b630d69e0a9334b76a0f8bef9bf
GabrielCoderz <biel.s.maia@hotmail.com>
2022-12-17 13:57:26 -0300

    update handler test

--- src/handler/index.ts
+++ src/handler/index.ts
@@ -5,7 +5,7 @@ import * as _ from 'lodash';
 export const handler: Handler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResultV2> => {
     const response = {
         statusCode: 200,
-        body: JSON.stringify('Hello from Lambda again!')
+        body: JSON.stringify('FUNCIONANDO LAMBDA FUNCTION SERVELESS!!!!')
     }
 
     return response;
